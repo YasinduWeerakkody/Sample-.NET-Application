083158eead38cd78083f8920c3f3c6b782054694
YasinduWeerakkody <127224509+YasinduWeerakkody@users.noreply.github.com>
2024-03-06 17:49:25 +0530

    .net-firstday

---
+++ Models/Brand.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata;
+
+namespace crudApp.Models
+{
+    public class Brand
+    {
+
+
+        public int ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public String? Category { get; set; }
+
+        public int IsActive { get; set; }
+
+
+
+
+    }
+}
---
+++ Models/BrandContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crudApp.Models
+{
+    public class BrandContext : DbContext
+    {
+
+        public BrandContext(DbContextOptions<BrandContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+
+    }
+}
